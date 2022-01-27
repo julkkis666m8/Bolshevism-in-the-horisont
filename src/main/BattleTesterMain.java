9b27431c257233464857b5634a0dcf7efa50b142
julkkis666 <43990909+julkkis666@users.noreply.github.com>
2022-01-27 15:44:28 +0200

    battleTester "gui update" and experimentation

--- src/main/BattleTesterMain.java
+++ src/main/BattleTesterMain.java
@@ -34,7 +34,9 @@ public class BattleTesterMain {
         Army armyGer = new Army(germany);
         Army armyPol = new Army(poland);
 
-        armyGer.addRegiment(new Regiment("Artillery",6, 1, 1.2,0.5, true));
+        armyGer.addRegiment(new Regiment("Artillery",6, 1, 1.2,0.5,true));
+        armyGer.addRegiment(new Regiment("Artillery",6, 1, 1.2,0.5,true));
+        armyGer.addRegiment(new Regiment("Artillery",6, 1, 1.2,0.5,true));
         armyGer.addRegiment(new Regiment("Artillery",6, 1, 1.2,0.5,true));
         armyGer.addRegiment(new Regiment("Artillery",6, 1, 1.2,0.5,true));
         armyGer.addRegiment(new Regiment("Artillery",6, 1, 1.2,0.5,true));
@@ -42,34 +44,66 @@ public class BattleTesterMain {
         armyGer.addRegiment(new Regiment("Guards",1,1,1.1,1.2,false));
         armyGer.addRegiment(new Regiment("Guards",1,1,1.1,1.2,false));
         armyGer.addRegiment(new Regiment("Guards",1,1,1.1,1.2,false));
+
+
+        armyPol.addRegiment(new Regiment("Artillery",6, 1, 1.2,0.5,true));
+        armyPol.addRegiment(new Regiment("Knight",2,1,1.2,1.3,true));
+        armyPol.addRegiment(new Regiment("Knight",2,1,1.2,1.3,true));
         for(int i = 0; i < 10 ; i++){
             armyGer.addRegiment(new Regiment("Regiment",1,1,1,1.1,false));
             armyPol.addRegiment(new Regiment("Regiment",1,1,1,1.1,false));
         }
-        armyPol.addRegiment(new Regiment("Regiment",1,1,1,1.1,false));
-        armyPol.addRegiment(new Regiment("Regiment",1,1,1,1.1,false));
-        armyPol.addRegiment(new Regiment("Regiment",1,1,1,1.1,false));
-        armyPol.addRegiment(new Regiment("Regiment",1,1,1,1.1,false));
-        armyPol.addRegiment(new Regiment("Hussar",4,2,1.1,0.9,false));
-        armyPol.addRegiment(new Regiment("Hussar",4,2,1.1,0.9,false));
-        armyPol.addRegiment(new Regiment("Hussar",4,2,1.1,0.9,false));
-        armyPol.addRegiment(new Regiment("Hussar",4,2,1.1,0.9,false));
-        BattleController bc = new BattleController();
+        armyGer.addRegiment(new Regiment("Hussar",4,2,1.1,0.9,false));
+        armyGer.addRegiment(new Regiment("Hussar",4,2,1.1,0.9,false));
 
+        armyPol.addRegiment(new Regiment("Regiment",1,1,1,1.1,false));
+        armyPol.addRegiment(new Regiment("Regiment",1,1,1,1.1,false));
+        armyPol.addRegiment(new Regiment("Regiment",1,1,1,1.1,false));
+        armyPol.addRegiment(new Regiment("Hussar",4,2,1.1,0.9,false));
+        armyPol.addRegiment(new Regiment("Hussar",4,2,1.1,0.9,false));
+        armyPol.addRegiment(new Regiment("Hussar",4,2,1.1,0.9,false));
+        armyPol.addRegiment(new Regiment("Hussar",4,2,1.1,0.9,false));
+        armyPol.addRegiment(new Regiment("Hussar",4,2,1.1,0.9,false));
+        armyPol.addRegiment(new Regiment("Hussar",4,2,1.1,0.9,false));
+
+        BattleController bc = new BattleController();
         bc.startBattle(armyGer,armyPol, null);
 
-        //startBattleGui(bc);  //prob have to do this in an other manner
+        startBattleGui(bc);  //prob have to do this in an other manner
 
 
-        String myString = "";
+        String myString;
         while(bc.ongoing()){
+
+            int str1 = (int) armyGer.getTotalManpower();
+            int str2 = (int) armyPol.getTotalManpower();
+            System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
             bc.tick();
+
+            printLine((int)(armyGer.getTotalManpower()/500));
+            System.out.println("German "+(int)(armyGer.getTotalManpower() - str1));
+            System.out.println(armyGer.toString());
+            printLine((int)(armyPol.getTotalManpower()/500));
+            System.out.println("Polish "+(int)(armyPol.getTotalManpower() - str2));
+            System.out.println(armyPol.toString());
+
             myString = "";
             myString += armyGer.toString()+"\n";
             myString += armyPol.toString()+"\n";
             JOptionPane.showMessageDialog(null, myString);
         }
 
+    }
+
+    private static void printLine(int x)
+    {
+        if(x > 0)
+        {
+            System.out.print("-");
+            printLine(x-1);
+        }else{
+            System.out.print(" ");
+        }
     }
 
     private static void startBattleGui(BattleController bc) {
